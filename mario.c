0303362005321c354042671080cfba26a3943986
M_Masoud <42962017+MahmoudMasoud-77@users.noreply.github.com>
2019-09-26 20:22:41 +0200

    pset1-problems-2019-x-mario-less

--- mario.c
+++ mario.c
@@ -1,37 +1,26 @@
-#include <cs50.h>
 #include <stdio.h>
-
+#include <cs50.h>
 int main(void)
 {
-    int h;
+    
+   int h;
     do
     {
         h = get_int("Height: ");
     }
     while (h < 1 || h > 8);
     
-    for(int i=1;i<=h;i++)
+    for (int i=1;i<=h;i++)
     {
-        for(int j=1;j<=h+i+1;j++)
-        { 
-            if(j>=1 && j<=h)
-            {
-                if(j<=h-i)
-                    printf(" ");
-                else
-                    printf("#");
-            }
-            else if(j==h+1)
-            {
-                printf("  ");
-            }
+        for(int j=1;j<=h;j++)
+        {
+            if(j<=h-i)
+                printf(" ");
             else
-            {
                 printf("#");
-            }
             
         }
         printf("\n");
     }
-
 }
+
